d8e7f69cacb5b62592e993f48a4074bd7e1085eb
segitigapascal <segitigapascal172@gmail.com>
2025-12-10 23:58:46 +0700

    Update 4_Pertemuan_1.py

--- dataheroes_app/pages/4_Pertemuan_1.py
+++ dataheroes_app/pages/4_Pertemuan_1.py
@@ -1,12 +1,1 @@
-import streamlit as st
 
-st.set_page_config(page_title="Pertemuan 1", page_icon="📊")
-
-st.title("📊 Pertemuan 1 — Pengumpulan dan Penyajian Data")
-
-st.write("""
-Pada pertemuan ini, siswa belajar:
-- Apa itu data
-- Jenis data
-- Cara mengumpulkan data
-""")
